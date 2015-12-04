0736448ecc7b4f0d24b182ef9c045f19794ea275
陈王彬 <vcinly@gmail.com>
2015-12-04 22:56:39 +0800

    add Gemfile and daily

---
+++ DHT11/daily.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'bundler'
+
+Bundler.require(:default)
+
+output = `sudo ./temp`
+match_data = output.match /RH:(?<RH>\d+),TEMP:(?<TEMP>\d+)/
+
+humidity = match_data['RH']
+temperature = match_data['TEMP']
+
+time = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+
+humidity_data = {timestamp: time, value: humidity}.to_s
+temperature_data = {timestamp: time, value: temperature}.to_s
+
+json_temperature = %Q[{\\"timestamp\\": \\"#{time}\\", \\"value\\": #{temperature}}]
+json_humidity = %Q[{\\"timestamp\\": \\"#{time}\\", \\"value\\": #{humidity}}]
+
+`curl -X POST -d "#{json_temperature}" -H "Content-Type: application/json" -H "U-ApiKey:9a820441dbb60753618fe667c625232f" http://api.yeelink.net/v1.0/device/342655/sensor/380108/datapoints`
+`curl -X POST -d "#{json_humidity}" -H "Content-Type: application/json" -H "U-ApiKey:9a820441dbb60753618fe667c625232f" http://api.yeelink.net/v1.0/device/342655/sensor/380676/datapoints`
+
+
